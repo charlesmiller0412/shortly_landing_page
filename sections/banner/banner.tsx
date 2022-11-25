96fe99705064cde7353451920fd0725e598dea43
Charles Miller <charles.miller.0412@gmail.com>
2022-11-25 17:07:47 -0600

    updated styling and semantic html

--- sections/banner/banner.tsx
+++ sections/banner/banner.tsx
@@ -2,9 +2,9 @@ import { Btn } from "../../components/btn";
 
 export const Banner = () => {
     return (
-        <div className="banner">
+        <section className="banner">
             <h3>Boost your links today</h3>
             <Btn text="get started" className="btn" href="#" />
-        </div>
+        </section>
     );
 };
