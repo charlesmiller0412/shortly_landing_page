f8ebf7ac6d11b962693e10dedbb1be5393ca1822
Charles Miller <charles.miller.0412@gmail.com>
2022-11-27 13:11:54 -0600

    fixed button onClick

--- sections/input/input.tsx
+++ sections/input/input.tsx
@@ -136,6 +136,7 @@ export const Input = () => {
                         id="formSubmit"
                         className="btnInput"
                         onClick={(e) => {
+                            e.preventDefault();
                             fetchLink();
                         }}
                     >
